8743616dcce36e3bf282320e99d88262f442b6db
lucaznch <lucazanchisilva@gmail.com>
2025-03-21 02:24:43 +0000

    chore(response-collector): add comments

--- Front-End/src/main/java/pt/ulisboa/tecnico/tuplespaces/frontend/ResponseCollector.java
+++ Front-End/src/main/java/pt/ulisboa/tecnico/tuplespaces/frontend/ResponseCollector.java
@@ -6,12 +6,23 @@ import java.util.List;
 
 /**
  * the ResponseCollector class is responsible for collecting the responses from the TupleSpaces servers
- * it collects the responses from the server(s) and provides a method to retrieve the collected responses
+ * it collects the responses from the servers through the observers
  */
 public class ResponseCollector {
 
-    private ArrayList<ResponseEntry> collectedHistory;
-    private ArrayList<ResponseLockEntry> collectedLockHistory;
+    private ArrayList<ResponseEntry> collectedHistory;          // table with all the responses received from the servers
+                                                                // | requestType | requestId | request | response | serverId |
+                                                                // |     PUT     |     1     |   ...   |   ...    |    0     |
+                                                                // |     PUT     |     1     |   ...   |   ...    |    1     |
+                                                                // |     PUT     |     1     |   ...   |   ...    |    2     |
+                                                                // |    READ     |     6     |   ...   |   ...    |    0     |
+
+    private ArrayList<ResponseLockEntry> collectedLockHistory;  // table with all the lock responses received from the servers
+                                                                // | requestType | requestId | request | response | serverId |
+                                                                // |     LOCK    |     1     |   ...   |   ...    |    0     |
+                                                                // |     LOCK    |     1     |   ...   |   ...    |    2     |
+                                                                // |    UNLOCK   |     1     |   ...   |   ...    |    0     |
+                                                                // |    UNLOCK   |     1     |   ...   |   ...    |    2     |
 
 
     public ResponseCollector() {
@@ -20,11 +31,27 @@ public class ResponseCollector {
     }
 
 
+    /**
+     * this method is used to add a response to the collected history
+     * @param requestType the type of the request (PUT, READ, TAKE)
+     * @param requestId the request ID
+     * @param request the request
+     * @param response the response
+     * @param serverId the server ID
+     */
     public synchronized void addResponse(String requestType, int requestId, String request, String response, int serverId) {
         this.collectedHistory.add(new ResponseEntry(requestType, requestId, request, response, serverId));
         notifyAll();
     }
 
+    /**
+     * this method is used to get the response for a given request ID
+     * NOTE: requests IDs are unique! in the table, there are 3 entries for each request ID, one for each server
+     * 
+     * @param requestId the request ID
+     * @param requestType the type of the request (PUT, READ, TAKE)
+     * @return the response for the given request ID
+     */
     public synchronized String getResponse(int requestId, String requestType) {
         if (requestType.equals("PUT")) {
             int requestsCounter = 0;
@@ -32,7 +59,7 @@ public class ResponseCollector {
 
             for (ResponseEntry e : this.collectedHistory) {
                 if (e.getRequestId() == requestId) {
-                    System.err.println("[\u001B[34mDEBUG\u001B[0m] entry = " + e.toString());
+                    // System.err.println("[\u001B[34mDEBUG\u001B[0m] entry = " + e.toString());
                     requestsCounter++;
                     if (e.getResponse().equals("OK")) { okCounter++; }
                     if (requestsCounter == 3) { break; }
@@ -42,15 +69,15 @@ public class ResponseCollector {
             else { return "NO"; }
         }
         else if (requestType.equals("READ")) {
-            for (ResponseEntry e : this.collectedHistory) {
-                if (e.getRequestId() == requestId) {
+            for (ResponseEntry e : this.collectedHistory) {     // READ waits only for 1 response
+                if (e.getRequestId() == requestId) {            // therefore we get the first response we find
                     return e.getResponse();
                 }
             }
         }
-        else if (requestType.equals("TAKE")) {
-            for (ResponseEntry e : this.collectedHistory) {
-                if (e.getRequestId() == requestId) {
+        else if (requestType.equals("TAKE")) {                  // TAKE waits for 3 responses
+            for (ResponseEntry e : this.collectedHistory) {     // but with Maekawa algorithm we can get consistency between servers
+                if (e.getRequestId() == requestId) {            // therefore we get the first response we find
                     return e.getResponse();
                 }
             }
@@ -60,7 +87,7 @@ public class ResponseCollector {
     }
 
     /**
-     * this method is used to wait until all the responses for a given request ID are received.
+     * this method is used to wait until N the responses for a given request ID are received.
      * @param requestId the request ID to wait for
      * @param n the number of responses to wait for
      */
@@ -94,11 +121,27 @@ public class ResponseCollector {
         }
     }
 
+    /**
+     * this method is used to add a lock response to the collected history
+     * @param requestType the type of the request (LOCK, UNLOCK)
+     * @param requestId the request ID
+     * @param request the request
+     * @param response the response
+     * @param serverId the server ID
+     */
     public synchronized void addLockResponse(String requestType, int requestId, String request, boolean response, int serverId) {
         this.collectedLockHistory.add(new ResponseLockEntry(requestType, requestId, request, response, serverId));
         notifyAll();
     }
 
+    /**
+     * this method is used to get the lock response for a given request ID
+     * NOTE: requests IDs are unique! in the table, there are 2 entries for each request ID, one for each server
+     * 
+     * @param requestId the request ID
+     * @param serverId the server ID
+     * @return the lock response for the given request ID
+     */
     public synchronized boolean getLockResponse(int requestId, int serverId) {
         int requestsCounter = 0;
 
@@ -111,6 +154,11 @@ public class ResponseCollector {
         return false;
     }
 
+    /**
+     * this method is used to wait until all the lock responses for a given request ID are received.
+     * @param requestId the request ID to wait for
+     * @param requestType the type of the request (LOCK, UNLOCK)
+     */
     public synchronized void waitUntilAllLockReceived(int requestId, String requestType) {
         int requestsCounter;
 
